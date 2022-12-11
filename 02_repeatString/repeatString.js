3a9c74a4a25f6148d5ec8ba3ebac5077823affe7
JezzeX <jezzejagz4@gmail.com>
2022-12-12 00:03:44 +0100

    Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    var sentence = "";
+    for (let i = 0; i < num; i++) {
+      sentence += string;
+    }
+    return sentence;
+  }
 };
 
 // Do not edit below this line
