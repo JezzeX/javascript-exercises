11995eacbb192d0ea7c8defca3f3c6457cf792b7
JezzeX <jezzejagz4@gmail.com>
2022-12-12 00:46:36 +0100

    Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  word = string.split("");
+  var length = word.length;
+  var reverseArray = [];
+  var reverse;
 
+  for (let i = 0; i < length; i++) {
+    reverseArray.push(word.pop());
+  }
+  reverse = reverseArray.join("");
+  return reverse;
 };
 
 // Do not edit below this line
