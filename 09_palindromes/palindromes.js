da4ada54ca8554117ed7806a64f1e782d644c6e7
JezzeX <jezzejagz4@gmail.com>
2023-01-01 15:03:31 +0100

    Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  var processedstr = str.toLowerCase().replace(/[^a-z]/g, "");
+  var reversestr = processedstr.split("").reverse().join("");
+  return reversestr === processedstr;
 };
 
 // Do not edit below this line
